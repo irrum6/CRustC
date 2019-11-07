02de6ea159df7def92786ba360470a951049b935
irakli Mindorashvili <iraklim@localhost.localdomain>
2019-11-07 17:16:05 +0400

    makefiles

--- swap/main.c
+++ swap/main.c
@@ -5,6 +5,7 @@ int main()
 {
     int a = 69;
     int b = 72;
+    printf("swap example using binary or\n");
     printf("a is %d, b is %d \n", a, b);
     printf("------swap------\n");
     swap(&a, &b);
