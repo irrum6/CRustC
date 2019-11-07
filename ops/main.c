b726f5e27056c72af3cc367bcfe9f5c31cf57b34
irakli Mindorashvili <iraklim@localhost.localdomain>
2019-11-07 17:38:48 +0400

    renamed ops

--- ops/main.c
+++ ops/main.c
@@ -13,8 +13,19 @@ int main()
     b = 5;
     c = 6;
     d = a + b + (a != b);
-    e = a + b * (a == b);// a==b is false is 0 b*0 is 0 result is 4
+    e = a + b * (a == b); // a==b is false is 0 b*0 is 0 result is 4
     f = a * b - (a <= b);
     printf("a:%d,b:%d,c:%d,d:%d,e:%d,f:%d\n", a, b, c, d, e, f);
+    //logical operators
+    a = 5;
+    b = 6;
+    c = 7;
+    d = (a > b);
+    e = (a < b);
+    f = d || e && (d || e);
+    printf("a:%d,b:%d,c:%d,d:%d,e:%d,f:%d\n", a, b, c, d, e, f);
+    f = e || d && (d || e); //hmm
+    printf("a:%d,b:%d,c:%d,d:%d,e:%d,f:%d\n", a, b, c, d, e, f);
+
     return 0;
 }
