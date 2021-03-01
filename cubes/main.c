f9fc218517e130d4d440b40c0e13086dba599bd5
irrum6 <irrum6@github.com>
2021-03-01 20:43:04 +0400

    count mu

--- cubes/main.c
+++ cubes/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void findCubes(unsigned int n);
+int findCubes(unsigned int n);
 int main()
 {
 
-    const int MAX = 1000;
-    int num = 0;
+    const unsigned int MAX = 1000;
+    unsigned int num = 0;
     printf("enter number between 1 and %d\n", MAX);
     scanf("%d", &num);
     if (num > MAX)
@@ -13,14 +13,16 @@ int main()
         printf("maxed\n");
         num = MAX;
     }
-    findCubes(num);
+    int found = findCubes(num);
+    printf("Found %d such numbers",found);
     return 0;
 }
 
-void findCubes(unsigned int n)
+int findCubes(unsigned int n)
 {
     //find and print every a,b,c,d < n
     //where a^3+b^3= c^3+ d^3
+    int count = 0;
     for (unsigned int i = 0; i < n; i++)
     {
         for (unsigned int j = 0; j < n; j++)
@@ -37,6 +39,7 @@ void findCubes(unsigned int n)
                     unsigned int cube2 = (k * k * k) + (l * l * l);
                     if (cube2 == cube)
                     {
+                        count++;
                         printf("cubes:\n", cube);
                         printf("%d,%d,%d,%d\n", i, j, k, l);
                     }
@@ -44,5 +47,6 @@ void findCubes(unsigned int n)
             }
         }
     }
+    return count;
 }
 // I feel stupid
